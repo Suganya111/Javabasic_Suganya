4b3352b1d544f7338aefd962fecb668aa4227cae
Suganya1110 <suganyasumathi123@gmail.com>
2021-12-04 04:40:30 -0800

    Addition

---
+++ src/com/aaludra/basicprogram/Addition.java
@@ -0,0 +1,12 @@
+package com.aaludra.basicprogram;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 2;
+		int j = 3;
+		System.out.println(i + j);
+	}
+
+}
