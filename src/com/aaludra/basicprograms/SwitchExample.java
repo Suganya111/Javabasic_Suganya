52af89ca264eb85c0f6a1a8a77911e62f870b2f6
suganya1110 <suganyasumathi123@gmail.com>
2021-12-06 03:50:33 +0530

    Switch Program

---
+++ src/com/aaludra/basicprograms/SwitchExample.java
@@ -0,0 +1,41 @@
+package com.aaludra.basicprograms;
+
+import java.util.Scanner;
+
+public class SwitchExample {
+	int a = 20;
+	int b = 30;
+	Scanner sc = new Scanner(System.in);
+	public void display() {
+		System.out.println("1. Addition");
+		System.out.println("2. Subtraction");
+		System.out.println("3. Multiplication");
+		System.out.println("4. Division");
+		System.out.println();
+		System.out.println("Enter your choice");
+		int choice = sc.nextInt();
+		switch (choice) {
+		case 1:
+			System.out.println(a + b);
+			break;
+		case 2:
+			System.out.println(a - b);
+			break;
+		case 3:
+			System.out.println(a * b);
+			break;
+		case 4:
+			System.out.println(a / b);
+			break;
+
+		}
+
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		SwitchExample example = new SwitchExample();
+		example.display();
+	}
+
+}
