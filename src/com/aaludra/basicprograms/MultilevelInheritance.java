645f0ad57e622801be9addc1772ac5f47f2ed3a3
suganya1110 <suganyasumathi123@gmail.com>
2021-12-07 05:03:21 +0530

    Multilevel Inheritance

---
+++ src/com/aaludra/basicprograms/MultilevelInheritance.java
@@ -0,0 +1,9 @@
+package com.aaludra.basicprograms;
+
+public class MultilevelInheritance extends InheritanceExample {
+	String designation = "Software Trainee";
+
+	public void show() {
+		System.out.println(EmployeeName + "," + EmployeeId + " designation in Aaludra Technologies is " + designation);
+	}
+}
