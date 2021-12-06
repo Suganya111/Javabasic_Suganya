bc130923d8eacf44138021f520bd85368efdf6e1
suganya1110 <suganyasumathi123@gmail.com>
2021-12-06 19:14:30 +0530

    Datatypes

---
+++ src/com/aaludra/basicprograms/DatatypeExample.java
@@ -0,0 +1,25 @@
+package com.aaludra.basicprograms;
+
+public class DatatypeExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 5;
+		float f = 10.45f;
+		String name = "Suganya";
+		String num = "1234";
+		char ch = 'A';
+		long mobileNum = 8870569353l;
+		int[] array = { 1, 2, 3, 4, 5 };
+		for (int j = 0; j < array.length; j++) {
+			System.out.print(array[j] + " ");
+		}
+		System.out.println();
+			System.out.println("Integer Value is " + i + "\nFloat value is " + f + "\nString value is " + name
+					+ "\nString num " + num + "\nCharacter value " + ch + "\nMobile number is " + mobileNum);
+
+		}
+	}
+
+
+
