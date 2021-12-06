17bdae887620dc29f6718c800e7a9287360ba0fc
suganya1110 <suganyasumathi123@gmail.com>
2021-12-06 18:59:55 +0530

    Variables

---
+++ src/com/aaludra/basicprograms/VariableExample.java
@@ -0,0 +1,22 @@
+package com.aaludra.basicprograms;
+
+public class VariableExample {
+	static String name = "Aaludra"; // static variable can be used anywhere in the class
+	int i = 5;// instance variable is created when object is created and destroyed when obj is
+				// destroyed
+
+	public void print() {
+		int id = 4509;
+		name = "saranya";
+		System.out.println(i + "\n" + name);
+		System.out.println(id);// local variable can be accessed only by its method
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		VariableExample value = new VariableExample();
+		value.print();
+	}
+
+}
