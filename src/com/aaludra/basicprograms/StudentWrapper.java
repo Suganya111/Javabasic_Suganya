9e17c1231fadbdae3dcf4ce090d09e137d5ec1b5
suganya1110 <suganyasumathi123@gmail.com>
2021-12-15 12:51:00 +0530

    WrapperClass with Collections

---
+++ src/com/aaludra/basicprograms/StudentWrapper.java
@@ -0,0 +1,27 @@
+package com.aaludra.basicprograms;
+
+public class StudentWrapper implements Comparable<StudentWrapper> {
+	Integer rollno;
+	String name;
+	Double marks;// Wrapper class is used because of using comparable interface for sorting out
+
+	StudentWrapper(int rollno, String name, double marks) {
+		this.rollno = rollno;
+		this.name = name;
+		this.marks = marks;
+	}
+
+	@Override
+	public int compareTo(StudentWrapper s) {
+		return this.rollno.compareTo(s.rollno);
+
+	}
+
+	@Override
+	public String toString() {
+		  return "StudentWrapper{"
+		            + "RollNo=" + rollno + ", Name='" + name + '\''
+		            + ", Marks=" + marks + '}';
+	
+		}
+	}
